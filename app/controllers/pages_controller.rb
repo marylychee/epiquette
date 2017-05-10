902ad8d898f08797ec5101d859e6e00e6b443f71
Andrealiz Afable <dreafable@gmail.com>
2017-05-09 20:25:16 -0700

    add controllers, pages, and change routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+
+  def home
+    
+  end
+
+  def about_us
+    
+  end
+
+  def contact_us
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/' => 'pages#home'
+  get '/about_us' => 'pages#about_us'
+  get '/contact_us' => 'pages#contact_us'
+
 end
